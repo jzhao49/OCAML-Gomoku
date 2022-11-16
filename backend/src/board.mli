003f2a3666b2a6ed599191df2dabbe357f157a19
Mihir2000 <mihir.chakravarthi@gmail.com>
2022-11-16 17:26:06 -0500

    changes

--- backend/src/board.mli
+++ backend/src/board.mli
@@ -1,29 +1,74 @@
 open Core
 
-module Vec: sig
+module Coordinates: sig
   type t = (int * int) [@@deriving compare, sexp]
 end 
 
 module Board: sig
-  (* Map where the keys are vectors of 2 elements *)
-  module VecMap: Map.S with type Key.t = Vec.t
+  (* 
+     Map where the keys are Coordinates of 2 elements 
+  *)
+  module CoordMap: Map.S with type Key.t = Coordinates.t = Map.Make(Coordinates)
 
-  (* Type representing the position of something on the board (x, y, c = player color) *)
-  type position = {x: int, y: int, c: char} [@@deriving yojson]
+  (* 
+     Type representing the position of something on the board (x, y, player = 0 or 1) 
+  *)
+  type player_number = int
 
-  (* A vector of pieces *)
-  type pieces = (Char.t) Vec.t
+  type position = {
+    x: int, y: int, player: player_number} 
+    [@@deriving yojson]
 
-  type win_dirs = (Vec.t) List.t
+  (* 
+     A map of pieces on the board to their respective player
+  *)
+  type pieces = (Int.t) CoordMap.t
+  (*
+     Type representing a list of Coordinates to store possible win positions,
+     empty positions, etc. 
+  *)
 
+  type possible_positions = (Coordinates.t) List.t
+
+  (* 
+    Print out board using yojson
+  *)
   val yojson_of_pieces: pieces -> string
+
+  (*
+     List of valid locations on the board left to play pieces
+  *)
+  val valid_locations: pieces -> possible_positions
+
+  (* 
+    List of locations on the board where playing a piece would 
+    result in a win. Used to check with game_over
+  *)
+  val win_locations: pieces -> possible_positions
   
-  (* Inserts a piece onto the board if the position is valid. Returns an error otherwise *)
-  val insert: pieces -> Vec.t -> char -> (pieces, pieces) new_board
+  (* 
+     Inserts a piece onto the board if the position is valid. Returns an error otherwise 
+  *)
+  val insert: pieces -> Coordinates.t -> player_number -> (pieces, pieces) new_board
 
-  val longest: pieces -> Vec.t -> Vec.t -> int * (Vec.t list)
+  (* 
+    Returns a list of all the lines players have
+  *)
 
-  (* Checks whether a certain player has won (white, black) *)
-  val game_over: pieces -> win_dirs -> Vec.t -> char -> (bool, bool) result
+  val list_of_player_lines: pieces -> player_number -> Coordinates.t list list
+
+  (* 
+     takes in list of all the lines players have and outputs their longest line 
+  *)
+
+  val longest: Coordinates.t list list -> player_number -> int * (Coordinates.t list)
+
+  (* 
+     Inserts a piece and then checks whether a certain player has won (0 or 1) 
+  *)
+
+  val game_over: pieces -> win_locations -> Coordinates.t -> player_number -> (bool, player_number) result
+
+
 
 end 
