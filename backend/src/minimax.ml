aee1747206d5081ace77a8dd7bc405d1676c3c86
Mihir2000 <mihir.chakravarthi@gmail.com>
2022-12-16 17:45:56 -0500

    small changes

--- backend/src/minimax.ml
+++ backend/src/minimax.ml
@@ -8,10 +8,25 @@ module CoordMap = Game.CoordMap
    time to compute *)
 let difficulty = 3
 
+let rec create_coordinates_ls x y acc =
+  if x > 15 then acc
+  else if y > 15 then create_coordinates_ls (x + 1) 0 acc
+  else create_coordinates_ls x (y + 1) ((x,y)::acc)
+let coordinates_ls = create_coordinates_ls 0 0 []
+let init_map = 
+  CoordMap.of_alist_exn(List.map coordinates_ls ~f:(fun coordinate -> (coordinate, 0)))
+
+(* let update_map (init : Game.pieces_map) (pos: Coordinates.t) (player_num : int) =
+  CoordMap.add_exn init ~key:(pos) ~data:player_num *)
+
+let update_map (init : Game.pieces_map) (cur_board : Game.pieces_map) : Game.pieces_map =
+  CoordMap.fold cur_board ~init:init ~f:(fun ~key:pos ~data:player_num acc ->
+    CoordMap.add_exn acc ~key:(pos) ~data:player_num)
+
 (* eval returns a positive value if current player is winning,
    negative for the other, and 0 otherwise *)
 let eval (p : Game.pieces_map) (cur_player : int) : int =
-  let directions = [ (0, 1); (1, 0); (1, 1); (-1, 1) ] in
+  (* let directions = [ (0, 1); (1, 0); (1, 1); (-1, 1) ] in *)
   let player_2 = if cur_player = 1 then 2 else 1 in
   let score =
     CoordMap.fold p ~init:0 ~f:(fun ~key:_ ~data:player acc ->
@@ -32,13 +47,15 @@ let insert_piece_no_error (p : Game.pieces_map) ((x, y) : Coordinates.t)
 
 let rec minimax (p : Game.pieces_map) (cur_player : int) (depth : int) :
     Coordinates.t * int =
+  let cur_board = update_map init_map p in
   if depth = 0 then
     (* Max depth has been reach so return *)
     ((-1, 1), eval p cur_player)
   else
     let available_positions =
-      CoordMap.fold p ~init:[] ~f:(fun ~key:pos ~data:player acc ->
-          if player = 0 then pos :: acc else acc)
+      CoordMap.fold cur_board ~init:[] ~f:(fun ~key:pos ~data:player acc ->
+          if player = 0 then 
+            pos :: acc else acc)
     in
     if List.is_empty available_positions then ((-1, 1), eval p cur_player)
       (* if no more available pos then return cur board eval *)
