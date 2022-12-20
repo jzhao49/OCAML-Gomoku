3373534fd2c4afb8c353537a0c2207a6f0efe2bf
Mihir2000 <mihir.chakravarthi@gmail.com>
2022-12-20 15:13:59 -0500

    implemented option for player to go either 1st or 2nd vs ai

--- backend/src/minimax.ml
+++ backend/src/minimax.ml
@@ -2,11 +2,6 @@ open Core
 open Board
 module CoordMap = Game.CoordMap
 
-(* the value of difficulty determines how many moves ahead the AI
-   will consider before coming to a decision. A higher number will
-   a more advanced AI that makes smarter decisions that take a longer
-   time to compute *)
-(* let difficulty = 3 *)
 
 let rec create_coordinates_ls x y acc =
   if x > 14 then acc
@@ -19,62 +14,13 @@ let init_map =
   CoordMap.of_alist_exn
     (List.map coordinates_ls ~f:(fun coordinate -> (coordinate, 0)))
 
-(* let update_map (init : Game.pieces_map) (pos: Coordinates.t) (player_num : int) =
-   CoordMap.add_exn init ~key:(pos) ~data:player_num *)
 
 let update_map (init : Game.pieces_map) (cur_board : Game.pieces_map) :
     Game.pieces_map =
   CoordMap.fold cur_board ~init ~f:(fun ~key:pos ~data:player_num acc ->
       CoordMap.set acc ~key:pos ~data:player_num)
 
-(* eval returns a positive value if current player is winning,
-   negative for the other, and 0 otherwise *)
-(* let eval (p : Game.pieces_map) (cur_player : int) : int =
-   (* let directions = [ (0, 1); (1, 0); (1, 1); (-1, 1) ] in *)
-   let player_2 = if cur_player = 1 then 2 else 1 in
-   let score =
-     CoordMap.fold p ~init:0 ~f:(fun ~key:_ ~data:player acc ->
-         if player = cur_player then acc + 1
-         else if player = player_2 then acc - 1
-         else acc)
-   in
-   score * 10 *)
 
-
-(* let open_three_in_a_row (pieces_map : Game.pieces_map) (player : int) : int =
-  let directions = [ (0, 1); (1, 0); (1, 1); (-1, 1) ] in
-  (* Iterate over each position on the board and count the number of open three-in-a-row sequences for the given player *)
-  CoordMap.fold pieces_map ~init:0
-    ~f:(fun ~key:(x, y) ~data:player_at_pos acc ->
-      if player_at_pos = player then
-        (* Check for open three-in-a-row sequences in each direction and add to the count if found *)
-        List.fold directions ~init:acc ~f:(fun acc (dx, dy) ->
-            let left_pos = (x - dx, y - dy) in
-            let right_pos = (x + dx, y + dy) in
-            if
-              CoordMap.mem pieces_map left_pos
-              && CoordMap.find_exn pieces_map left_pos = 0
-              && CoordMap.mem pieces_map right_pos
-              && CoordMap.find_exn pieces_map right_pos = 0
-            then acc + 1
-            else acc)
-      else acc) *)
-
-(* 
-(* Eval returns -10000000 if opponent has 3 in a rows, and a positive number otherwise *)
-let eval (pieces_map : Game.pieces_map) (cur_player : int) : int =
-  let player_2 = if cur_player = 1 then 2 else 1 in
-  let opponent_open_three_in_a_row = open_three_in_a_row pieces_map player_2 in
-  if opponent_open_three_in_a_row > 0 then
-    (* Subtract a large negative number to prioritize preventing the opponent from winning over trying to win ourselves *)
-    -1000000
-  else
-    (* Calculate the number of open three-in-a-row sequences for the current player and the opponent, and subtract the opponent's count from the current player's count *)
-    open_three_in_a_row pieces_map cur_player
-    - open_three_in_a_row pieces_map player_2 *)
-
-(* Minimax uses a minimax algorithm to return the best move and
-   score for the AI *)
 
 let insert_piece_no_error (p : Game.pieces_map) ((x, y) : Coordinates.t)
     (player : int) : Game.pieces_map =
@@ -87,40 +33,6 @@ let available_positions (p : Game.pieces_map) :
   let cur_board = update_map init_map p in
   CoordMap.fold cur_board ~init:[] ~f:(fun ~key:pos ~data:player acc ->
       if player = 0 then pos :: acc else acc)
-(* 
-let rec minimax (p : Game.pieces_map) (cur_player : int) (depth : int) :
-    Coordinates.t * int =
-  let available_positions_ls = available_positions p in
-  if depth = 0 then
-    (* Max depth reached *)
-    ((-1, -1), eval p cur_player)
-  else if List.is_empty available_positions_ls then
-    ((-1, -1), eval p cur_player)
-    (* if no more available pos then return cur board eval *)
-  else
-    (* compute score for each empty pos and choose the best *)
-    let best_move, high_score =
-      List.fold available_positions_ls
-        ~init:(List.hd_exn available_positions_ls, -100000000)
-        ~f:(fun (best_move, high_score) pos ->
-          let dummy_p = insert_piece_no_error p pos cur_player in
-          let _, score =
-            minimax dummy_p (if cur_player = 1 then 2 else 1) (depth - 1)
-          in
-          if cur_player = 1 then
-            if score > high_score then (pos, score) else (best_move, high_score)
-          else if score < high_score then (pos, score)
-          else (best_move, high_score))
-    in
-    (best_move, high_score) *)
-
-(* let ai_move (p : Game.pieces_map) (cur_player : int) : Game.pieces_map =
-   let best_move, _ = minimax p cur_player difficulty in
-   insert_piece_no_error p best_move cur_player *)
-
-(* let ai_move (p : Game.pieces_map) (cur_player : int) : Coordinates.t =
-  let best_move, _ = minimax p cur_player 3 in
-  best_move *)
 
 
 (* Used for alpha beta pruning for Minimax *)
@@ -238,7 +150,7 @@ let rec minimax_ab (state : board_state) (depth : int) (is_max_branch : bool) (a
       let new_state = {board = new_board; player = 2} in
       let value = minimax_ab new_state (depth - 1) (not is_max_branch) alpha beta in
       best_value := max !best_value value;
-      alpha := max !alpha value;
+      if !best_value > !alpha then alpha := max !alpha value;
       (* Prune the search if alpha is greater than or equal to beta *)
       (* There's no good way to break out of an iterator in OCAML so idk what to do here *)
       (* if !alpha >= !beta then List.stop (); *)
@@ -255,7 +167,7 @@ let rec minimax_ab (state : board_state) (depth : int) (is_max_branch : bool) (a
       let new_state = {board = new_board; player = 1} in
       let value = minimax_ab new_state (depth - 1) (not is_max_branch) alpha beta in
       best_value := min !best_value value;
-      beta := min !beta value;
+      if !best_value < !beta then beta := min !beta value;
       (* Prune the search if beta is less than or equal to alpha *)
       (* There's no good way to break out of an iterator in OCAML so idk what to do here *)
       (* if !beta <= alpha then CoordMap.stop (); *)
--- backend/src/play.ml
+++ backend/src/play.ml
@@ -2,13 +2,14 @@ open Core
 open Board
 module CoordMap = Game.CoordMap
 
-let make_move (pieces : Game.pieces_map) (move : Coordinates.t) (ai : bool)
+let make_move (pieces : Game.pieces_map) (move : Coordinates.t) (ai : bool) (ai_first : bool)
     (current_player : int) : int * Game.pieces_map =
   match Game.insert_piece pieces move current_player with
   | Ok new_pieces -> (
       match Game.game_over move current_player new_pieces with
       | true, current_player ->
           if ai && current_player = 2 then print_endline (sprintf "AI wins")
+          else if ai_first && current_player = 1 then print_endline (sprintf "AI wins")
           else print_endline (sprintf "Player %d wins!" current_player);
           (1, new_pieces)
       | _ -> (2, new_pieces))
@@ -26,7 +27,7 @@ let rec play_gomoku (pieces : int CoordMap.t) (ai : bool) (current_player : int)
          ai then (
         print_string "AI Move";
         let move = Minimax.ai_move pieces 2 in
-        match make_move pieces move ai current_player with
+        match make_move pieces move ai false current_player with
         | 1, _ -> print_endline (sprintf "Game over")
         | 2, new_pieces ->
             play_gomoku new_pieces ai (if current_player = 1 then 2 else 1)
@@ -42,7 +43,7 @@ let rec play_gomoku (pieces : int CoordMap.t) (ai : bool) (current_player : int)
           let y = Int.of_string (List.nth_exn move_list 1) in
           (x, y)
         in
-        match make_move pieces user_move ai current_player with
+        match make_move pieces user_move ai false current_player with
         | 1, _ -> print_endline (sprintf "Game over")
         | 2, new_pieces ->
             play_gomoku new_pieces ai (if current_player = 1 then 2 else 1)
@@ -57,11 +58,42 @@ let rec play_gomoku (pieces : int CoordMap.t) (ai : bool) (current_player : int)
         let y = Int.of_string (List.nth_exn move_list 1) in
         (x, y)
       in
-      match make_move pieces user_move ai current_player with
+      match make_move pieces user_move ai false current_player with
       | 1, _ -> print_endline (sprintf "Game over")
       | 2, new_pieces ->
           play_gomoku new_pieces ai (if current_player = 1 then 2 else 1)
       | 3, pieces -> play_gomoku pieces ai current_player
+      | _ -> print_endline (sprintf "Won't reach this"))
+  | _ -> print_endline "Won't reach this"
+
+let rec play_gomoku_ai (pieces : int CoordMap.t) (current_player : int)
+    : unit =
+  (* Print the current state of the board *)
+  print_endline (Game.print_board pieces);
+  match current_player with
+  | 1 ->(
+        print_string "AI Move";
+        let move = Minimax.ai_move pieces 2 in
+        match make_move pieces move true true current_player with
+        | 1, _ -> print_endline (sprintf "Game over")
+        | 2, new_pieces ->
+            play_gomoku_ai new_pieces  (if current_player = 1 then 2 else 1)
+        | 3, pieces -> play_gomoku_ai pieces current_player
+        | _ -> print_endline (sprintf "Won't reach this"))
+  | 2 -> (
+      print_string "Player 1 Enter move: ";
+      let user_move : Coordinates.t =
+        let move_str = In_channel.input_line In_channel.stdin in
+        let move_list = String.split (Option.value_exn move_str) ~on:' ' in
+        let x = Int.of_string (List.nth_exn move_list 0) in
+        let y = Int.of_string (List.nth_exn move_list 1) in
+        (x, y)
+      in
+      match make_move pieces user_move true true current_player with
+      | 1, _ -> print_endline (sprintf "Game over")
+      | 2, new_pieces ->
+          play_gomoku_ai new_pieces (if current_player = 1 then 2 else 1)
+      | 3, pieces -> play_gomoku_ai pieces current_player
       | _ -> print_endline (sprintf "Won't reach this"))
   | _ -> print_endline "Won't reach this"
 
@@ -72,5 +104,6 @@ let () =
       match int_of_string n with
       | 0 -> play_gomoku CoordMap.empty false 1
       | 1 -> play_gomoku CoordMap.empty true 1
+      | 2 -> play_gomoku_ai CoordMap.empty 1
       | _ -> print_endline "Invalid command")
   | _ -> Stdio.printf "\n"
